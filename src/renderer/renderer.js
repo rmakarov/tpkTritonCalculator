38456e2440f09b9f111c5fc4f336bc9610e3aa61
RamZek158 <extaxdead@gmail.com>
2026-07-12 19:48:31 +0300

    Adding tabs and a search

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,22 +1,21 @@
-
 document.getElementById("xlf").addEventListener("change", async (e) => {
   const file = e.target.files[0];
   if (!file) return;
 
-  console.log('[RENDERER] Файл выбран:', file.name); // 👈 проверка
+  console.log("[RENDERER] Файл выбран:", file.name); // 👈 проверка
 
   try {
     // Читаем файл как ArrayBuffer
     const buffer = await file.arrayBuffer();
-    console.log('[RENDERER] ArrayBuffer получен, размер:', buffer.byteLength);
+    console.log("[RENDERER] ArrayBuffer получен, размер:", buffer.byteLength);
 
     // Отправляем в main process
     const data = await window.excelAPI.readFile(buffer);
-    console.log('[RENDERER] Данные из main:', data);
+    console.log("[RENDERER] Данные из main:", data);
 
     document.getElementById("output").textContent = JSON.stringify(data, null, 2);
   } catch (err) {
-    console.error('[RENDERER] Ошибка:', err);
-    document.getElementById("output").textContent = 'Ошибка: ' + err.message;
+    console.error("[RENDERER] Ошибка:", err);
+    document.getElementById("output").textContent = "Ошибка: " + err.message;
   }
 });
